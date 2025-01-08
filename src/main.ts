413e9d456b29381645bfd7ffb68befc0558b9673
adee012 <adedwiputra16@gmail.com>
2025-01-08 12:32:46 +0700

    [UPDATE] update readme.md

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   dotenv.config();
@@ -27,6 +28,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
+
+  app.use('/', (req: Request, res: Response) => {
+    res.redirect('/api-docs');
+  });
 
   await app.listen(4040);
 
